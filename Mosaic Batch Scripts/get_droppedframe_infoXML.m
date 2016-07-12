3aac16bab4f60fbc5bb65d60136b2f7f73895d62
wmau <wmau@bu.edu>
2016-07-12 17:53:08 -0400

    Smarter way to look for dropped frames

--- Mosaic Batch Scripts/get_droppedframe_infoXML.m
+++ Mosaic Batch Scripts/get_droppedframe_infoXML.m
@@ -5,7 +5,7 @@ function [nFrames,nDropped,droppedInds] = get_droppedframe_infoXML(varargin)
 
 %% Import XML files. 
     if isempty(varargin)
-        file = file_select_batch('*.xml'); 
+        file.path = uigetfile('*.xml'); 
     else 
         for thisFile=1:length(varargin)
             file(thisFile).path = varargin{thisFile};
@@ -13,17 +13,24 @@ function [nFrames,nDropped,droppedInds] = get_droppedframe_infoXML(varargin)
     end
     
     nFiles = length(file); 
-    
-    %Hack. Looks like the xml files are stereotyped enough that I can just
-    %specify a row number for them. 
-    droppedRow = 13;
-    nDroppedRow = 12;
-    nFramesRow = 11; 
+     
+%     %Hack. Looks like the xml files are stereotyped enough that I can just
+%     %specify a row number for them. 
+%     droppedRow = 13;
+%     nDroppedRow = 12;
+%     nFramesRow = 11; 
     
     for thisFile=1:nFiles
         f = fopen(file(thisFile).path); 
         file(thisFile).data = textscan(f,'%s','Delimiter','\n');
         
+        dropped = find(~cellfun('isempty',strfind(file.data{1},'dropped')));
+        droppedRow = dropped(2);
+        nDroppedRow = dropped(1); 
+
+        nFrames = find(~cellfun('isempty',strfind(file.data{1},'frames')));
+        nFramesRow = nFrames(1); 
+
         nFrames = str2num(file(thisFile).data{1}{nFramesRow}(21:end-7));
         nDropped = str2num(file(thisFile).data{1}{nDroppedRow}(28:end-7));
         droppedInds = str2num(file(thisFile).data{1}{droppedRow}(22:end-7));
